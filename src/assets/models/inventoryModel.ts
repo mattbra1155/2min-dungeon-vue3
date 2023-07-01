8dabfd0f8d2c0a86307df494be97d3a54340bd99
mattbra1155 <mattbra1155@gmail.com>
2023-07-01 22:47:30 +0200

    added wieldItem func, wip equipItem func

--- src/assets/models/inventoryModel.ts
+++ src/assets/models/inventoryModel.ts
@@ -1,16 +1,53 @@
 import { IInventory } from '@/interfaces/IInventory'
 import { IArmor, IPotion, IWeapon } from '@/interfaces/IItem'
+import { IMonster } from '@/interfaces/IMonster'
+import { IPlayer } from '@/interfaces/IPlayer'
+import { Armor, Weapon } from './itemsModel'
+import { EItemCategory } from '@/enums/ItemCategory'
+import itemMods from '@/assets/json/itemMods.json'
 
 class Inventory implements IInventory {
-    constructor(public inventory: Array<IWeapon | IArmor | IPotion> = []) {
+    constructor(public inventory: Array<IWeapon | IArmor | IPotion> = [], public owner: IPlayer | IMonster) {
         this.inventory = inventory
+        this.owner = owner
     }
 
-    addToInventory(item: IArmor | IWeapon | IPotion): void {
+    addItem(item: IArmor | IWeapon | IPotion): void {
         if (!item) {
             return
         }
         this.inventory.push(item)
+    }
+    removeItem(itemId: number): void {
+        if (!itemId) {
+            return
+        }
+        const indexToRemove = this.inventory.findIndex((element) => element.id === itemId)
+        this.inventory.splice(indexToRemove, 1)
+    }
+    getItem(itemId: number) {
+        if (!itemId) {
+            return
+        }
+        return this.inventory.find((element) => element.id === itemId)
+    }
+    wieldItem(itemId: number) {
+        if (!itemId) {
+            return
+        }
+        const weapon = this.getItem(itemId)
+        if (weapon instanceof Weapon) {
+            this.owner.weapon = weapon
+        } else {
+            console.error(`${weapon?.name} not a weapon!!`)
+        }
+    }
+    equipItem(itemId: number) {
+        if (!itemId) {
+            return
+        }
+        const armor = this.getItem(itemId)
+        /// TO DO REST
     }
 }
 
--- src/interfaces/IInventory.ts
+++ src/interfaces/IInventory.ts
@@ -1,5 +1,7 @@
 import { IArmor, IPotion, IWeapon } from '@/interfaces/IItem'
-
+import { IPlayer } from '@/interfaces/IPlayer'
+import { IMonster } from '@/interfaces/IMonster'
 export interface IInventory {
     inventory: Array<IWeapon | IArmor | IPotion>
+    owner: IPlayer | IMonster
 }
