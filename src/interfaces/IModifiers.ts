63057c966f381c74b96fe314b561080bd4e36c93
Mateusz Brania <mbrania@x-one.com.pl>
2023-09-16 13:36:47 +0200

    WIP

--- src/interfaces/IModifiers.ts
+++ src/interfaces/IModifiers.ts
@@ -3,7 +3,6 @@ import { IStats } from '@/interfaces/IStats'
 import { MonsterModel } from '@/assets/models/monsterModel'
 import { PlayerModel } from '@/assets/models/playerModel'
 import { Modifiers } from '@/assets/models/modifiersModel'
-import { diceRollK100 } from '@/assets/scripts/diceRoll'
 
 export type IModifiersList = Partial<IStats> | Partial<{ damage: number; encumberence: number }>
 
@@ -18,6 +17,7 @@ export interface IModifierItem {
 
 export interface IModifierDamageOverTime extends Omit<IModifierItem, 'modifiers'> {
     chanceToApply: number | null
+    effectId: string
     duration: {
         isActive: boolean
         current: number | undefined
